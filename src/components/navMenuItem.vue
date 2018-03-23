cc0dca57bc5de52853092fd0d536c36ae32235d3
Andrew Zigler <andrewzigler@gmail.com>
2018-03-22 17:54:12 -0700

    Initialize top navigation menu

---
+++ src/components/navMenuItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <router-link v-on:click.native="closeMenu" class="navbar-item" :to="url">
+    <div class="menu-item">
+      <b-icon
+        :pack="pack"
+        :icon="icon"
+        :size="size"
+        :type="type"
+      ></b-icon>
+      <h2>{{text}}</h2>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'navMenuItem',
+  props: ['url', 'pack', 'icon', 'size', 'type', 'text'],
+  methods: {
+    closeMenu: function () {
+      this.navIsActive = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.menu-item {
+  justify-content: center;
+  display: flex;
+
+  .icon {
+    padding-top: 0.375rem;
+    margin-right: 0.5625rem;
+  }
+}
+
+.navbar-item {
+  &:hover {
+    span {
+      color: $accent!important;
+    }
+  }
+}
+</style>
---
+++ src/components/topNav.vue
@@ -0,0 +1,103 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <b-icon
+          pack="fab"
+          icon="quinscape"
+          size="is-small"
+          type="is-light"
+          class="navbar-item">
+        </b-icon>
+        <h1>Ophion</h1>
+      </router-link>
+      <div class="navbar-burger burger" @click="toggleMenu" :class="{'is-active': navIsActive}" data-target="mainNav">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div id="mainNav" class="navbar-menu is-bold is-primary" :class="{'is-active': navIsActive}">
+      <div class="navbar-end">
+        <nav-menu-item v-for="(item, index) in menu" :key="index" v-bind="menu[index]"></nav-menu-item>
+        <a class="navbar-item">
+          <div class="menu-item">
+            <b-icon
+              pack="fab"
+              icon="discord"
+              size="is-small"
+              type="is-light"
+            ></b-icon>
+            Discord
+          </div>
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import navMenuItem from '@/components/navMenuItem'
+
+const aboutMenuItem = {
+  url: '/about',
+  pack: 'fa',
+  icon: 'indent',
+  size: 'is-small',
+  type: 'is-light',
+  text: 'About'
+}
+
+const guideMenuItem = {
+  url: '/guide',
+  pack: 'fa',
+  icon: 'book',
+  size: 'is-small',
+  type: 'is-light',
+  text: 'Guide'
+}
+
+export default {
+  name: 'topMenu',
+  data () {
+    return {
+      navIsActive: false,
+      menu: [
+        aboutMenuItem,
+        guideMenuItem
+      ]
+    }
+  },
+  methods: {
+    toggleMenu: function () {
+      this.navIsActive = !this.navIsActive
+    }
+  },
+  components: {'nav-menu-item': navMenuItem}
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar-burger {
+color: $white;
+margin-top: 0.3rem;
+}
+
+.menu-item {
+  justify-content: center;
+  display: flex;
+
+  .icon {
+    padding-top: 0.375rem;
+    margin-right: 0.5625rem;
+  }
+}
+
+.navbar-item {
+  &:hover {
+    span {
+      color: $accent!important;
+    }
+  }
+}
+</style>
