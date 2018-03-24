27b4ba71504424c44762f35d2769e9128822540f
Andrew Zigler <andrewzigler@gmail.com>
2018-03-23 23:26:00 -0700

    Clean up App styles

--- src/App.vue
+++ src/App.vue
@@ -21,20 +21,19 @@ export default {
 <style lang='scss'>
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
-// Prevent overscroll
 html {
+  // Prevent overscroll
   overflow-y: hidden!important;
   height: 100%;
 }
+
 body {
+  // Prevent overscroll
   height: 100%;
   overflow: auto;
-}
-
-// Base styles
-body {
   background-image: linear-gradient(141deg, #dfd8d9 0%, whitesmoke 71%, white 100%);
 }
+
 #app {
   font-family: 'Source Sans Pro', sans-serif;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from '@/App'
+import router from '@/router'
 import Buefy from 'buefy'
+import Ophion from '@/lib'
 
 Vue.use(Buefy)
+Vue.use(Ophion)
 
 /* eslint-disable no-new */
 new Vue({
