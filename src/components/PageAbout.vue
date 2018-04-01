79ff79911b50cb6f716a47358383bf3e926405fd
Andrew Zigler <andrewzigler@gmail.com>
2018-03-31 23:12:39 -0700

    Initialize secondary pages

---
+++ src/components/PageAbout.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <section>
+      <p class="about">About - Coming Soon!</p>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'PageAbout'
+}
+</script>
---
+++ src/components/PageGuide.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <section>
+      <span>Guide - Coming Soon!</span>
+      <div class="tile is-ancestor">
+        <div class="tile is-4 is-vertical is-parent">
+          <div class="tile is-child box">
+            <p class="title">One</p>
+          </div>
+          <div class="tile is-child box">
+            <p class="title">Two</p>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <div class="tile is-child box">
+            <p class="title">Three</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'PageGuide'
+}
+</script>
+
+<style lang="scss" scoped>
+// Base styles
+main {
+  margin: 0.8rem 0.8rem 4rem 0.8rem;
+
+  .box {
+    background-color: rgba(173, 193, 188, 0.6);
+  }
+}
+</style>
